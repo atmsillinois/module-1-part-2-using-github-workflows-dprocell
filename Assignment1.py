884c701d6aa298d85dcc90e54f8777cb334a05ef
Dara Procell <procell2@illinois.edu>
2025-08-29 20:34:51 +0000

    starting assignment 1

---
+++ Assignment1.py
@@ -0,0 +1,14 @@
+# Assignment 1
+# Dara Procell
+# August 29, 2025
+
+'''
+Takes user inputs, uses a function to calculate something, 
+and prints the inputs and outputs to the screen.  Make sure your 
+code is formatted according to PEP 8, and documented using PEP 257.
+Every time you attempt to run the program or make a series of edits, 
+commit your changes to GitHub with descriptive commit messages.  
+For full credit, create at least one branch.  Add new functionality, 
+commit, test, and then create a pull request and add it to your main 
+branch. Add an open source software license  
+'''
